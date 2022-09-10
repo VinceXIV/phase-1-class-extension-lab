8e6d833aa2515e5a7a5ba265af6094f83473ef9d
Vincent <vincent.makokha@student.moringaschool.com>
2022-09-10 21:22:18 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+
+class Polygon {
+    constructor(sides){
+        this.sides = sides
+    }
+
+    get countSides(){
+        return this.sides.length
+    }
+
+    get perimeter(){
+        return this.sides.reduce((acc, sideLength)=>{
+            return acc + sideLength
+        }, 0)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid(){
+        const perimeter = this.perimeter
+        return !Boolean(this.sides.find(side => {
+            return side > perimeter - side
+        }))
+    }
+}
+
+class Square extends Polygon {
+    get isValid(){
+        const expectedSideLength = this.perimeter/4
+
+        const unexpectedSide = this.sides.find(sideLength => {
+            return sideLength != expectedSideLength
+        })
+
+        return !Boolean(unexpectedSide)
+    }
+
+    get area(){
+        if(this.isValid){
+            return this.sides[0]**2
+        }
+    }
+}
